6c55b33087207ead949c4b49e4dbd629823c4ff2
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2019-10-17 13:10:36 -0400

    Update calculate metrics for params.txt

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,11 +6,8 @@ set -o pipefail
 # list output files to detect new or missing files
 ls -1
 
-# params.txt output should be identical
-md5sum *params.txt
-
-# only count lines for .seg, .seg.txt and correctedDepth.txt due to nondeterministic numeric fields
-find . \( -iname "*.seg" -o -iname "*.seg.txt" -o -iname "*.correctedDepth.txt" \) -exec wc -l {} \;
+# count lines for .seg, .seg.txt, correctedDepth.txt and params.txt due to nondeterministic numeric fields
+find . \( -iname "*.seg" -o -iname "*.seg.txt" -o -iname "*.correctedDepth.txt" -o -iname "*params.txt" \) -exec wc -l {} \;
 
 # check size of RData binary file
 du -h *.RData
