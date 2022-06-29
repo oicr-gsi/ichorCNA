c7866b8d25bf228841cd2efa5a533c3b268db488
Beatriz Lujan <bibilujan@gmail.com>
2022-06-29 13:54:29 -0400

    update regression test and json

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -3,19 +3,21 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# list output files to detect new or missing files
+#1. list output files to detect new or missing files
 ls -1
 
-# 2. check on ID.cna.seq, from which other files are derived . : cat ID.cna.seq | cut -f 1-8 | md5sum
+#2. check on ID.cna.seq, from which other files are derived . : cat ID.cna.seq | cut -f 1-8 | md5sum
 cat *.cna.seg | cut -f 1-8 | md5sum
 
 
 #3. check on params.txt, from which ploidy and purity values are taken : cat id.params.txt | cut -f1-5 | md5sum
 cat *.params.txt | cut -f 1-5 | md5sum
 
+#4. check metrics json
+cat *_metrics.json | md5sum
 
-# check size of RData binary file
+#5. check size of RData binary file
 find . -iname "*.RData" -size +0 -printf '%p found and non-zero file size\n'
 
-# calculate what report files are archived
+#6. calculate what report files are archived
 tar --list --file *_plots.tar.gz
